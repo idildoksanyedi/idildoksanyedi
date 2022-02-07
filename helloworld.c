f16361ec68f533277da88cea895c5d3433277158
idildoksanyedi <99190606+idildoksanyedi@users.noreply.github.com>
2022-02-07 17:00:44 +0300

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+
+	printf("Hello, world!"); // printf C programlama dilinde yazdýrma iþlemini gerçekleþtirir.
+	                         // Ayrýca ; iþaretini koymadýðýmýz takdirde sistem komutun bittiðini anlayamaz. 
+	return 0;
+}
